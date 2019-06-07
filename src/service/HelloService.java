ca490e56100bc70df218dc3faade5013ef0c6acb
Janio <janiojunior@gmail.com>
2019-06-07 10:54:44 -0300

    exemplo com parametro

--- src/service/HelloService.java
+++ src/service/HelloService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -17,8 +18,14 @@ public class HelloService {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Usuario teste() {
-		
 		return new Usuario(1, "Joás");
+	}
+	
+	@GET
+	@Path("usuario/{nome}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Usuario teste(@PathParam("nome") String nome) {
+		return new Usuario(1, nome);
 	}
 	
 	@GET
